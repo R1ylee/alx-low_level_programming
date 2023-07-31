4c60bf7428d3acd1b9b0dee0924e92fde425d45e
R1ylee <njaugrace88@gmail.com git config --global user.email njaugrace88@gmail.com>
2023-07-31 15:31:08 -0700

    task 8 update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,25 +2,29 @@
 #include <stdio.h>
 /**
  * print_diagsums - entry point
- * @a: input
- * @size: input
+ * @a: pointer to array
+ * @size: size of array
  * RETURN: always 0
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
+	int i, size1;
+	int sum1, sum2;
 
+	size1 = 0;
 	sum1 = 0;
 	sum2 = 0;
 
-	for (y = 0; y < size; y++)
+	size1 = (size * size) - 1;
+
+	for (i = 0; i <= size1; i = i + (size + 1))
 	{
-		sum1 = sum1 + a{y * size + y};
+		sum1 = sum1 + a[i];
 	}
 
-	for (y = size - 1; y >= 0; y--)
+	for (i = (size - 1); i < size1; i = i + (size - 1))
 	{
-		sum2 = a[y * size + (size - y - 1)];
+		sum2 = sum2 + a[i];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
