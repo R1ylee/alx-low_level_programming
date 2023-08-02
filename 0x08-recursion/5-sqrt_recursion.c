400c5bb44afbb281f5b88005297822cc4c38cc83
R1ylee <njaugrace88@gmail.com git config --global user.email njaugrace88@gmail.com>
2023-08-02 05:39:24 -0700

    task 5 update 2

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 1));
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
@@ -27,6 +27,6 @@ int actual_sqrt_recursion(int n, int i)
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-		return (1);
+		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
 }
