8d0e73f60b9f97b86da194c8b4c0b7e0ddbc63bb
R1ylee <njaugrace88@gmail.com git config --global user.email njaugrace88@gmail.com>
2023-08-03 01:57:30 -0700

    task 8 update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,6 +6,7 @@
  * @s2: pointer to string params
  * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
@@ -14,7 +15,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
-		return (*s2 = '\0');
+		return (*s2 == '\0');
 	}
 	if (*s2 == '*')
 	{
