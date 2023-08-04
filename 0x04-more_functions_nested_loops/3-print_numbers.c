b2c0652ba407d661336018a2e41490a94ee6cc13
R1ylee <njaugrace88@gmail.com git config --global user.email njaugrace88@gmail.com>
2023-08-04 04:09:11 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+	_putchar(c + '0');
+	}
+	_putchar('\n');
+}
