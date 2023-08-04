533bf404eea371e1916bab256dbb990487963099
R1ylee <njaugrace88@gmail.com git config --global user.email njaugrace88@gmail.com>
2023-08-04 03:44:33 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: variable
+ * @b: variable
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
